744d3191a8aca56614a7d87b5f5f43e08d3ffb3f
tblinstance <tblinstance@gmail.com>
2026-06-13 00:33:09 +0600

    deploy

--- src/deployment/backend/core/settings.py
+++ src/deployment/backend/core/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 AUTH_USER_MODEL = 'tblinc.User'
 
